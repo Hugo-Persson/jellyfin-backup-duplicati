a732f633bc701d43ee5468d4e7b75c1d656ca4ea
Hugo Persson <hugo.e.persson@gmail.com>
2025-02-01 08:09:01 +0100

    added starting backup info

--- run-backup.sh
+++ run-backup.sh
@@ -6,18 +6,6 @@ cleanup() {
 
 # Run cleanup function on EXIT
 trap cleanup EXIT
-systemctl stop jellyfin
-
-URL="$(cat ./backup-url.txt)"
-DUPLICATI_PASSPHRASE="$(cat ./duplicati-passphrase.txt)"
-
-output=$(duplicati-cli backup "$URL" \
-  "/var/lib/jellyfin" \
-  "/etc/jellyfin" \
-  --dblock-size=50mb \
-  --backup-name="jellyfin" \
-  --passphrase="$DUPLICATI_PASSPHRASE" \
-  --retention-policy="1W:1D,4W:1W,12M:1M" | tee /dev/tty | tail -n 10)
 
 discord_url="$(cat ./discord-url.txt)"
 
@@ -31,6 +19,21 @@ send_discord_notification() {
   # Send POST request to Discord Webhook
   curl -H "Content-Type: application/json" -X POST -d "$payload" $discord_url
 }
+
+send_discord_notification "Starting Jellyfin backup"
+
+systemctl stop jellyfin
+
+URL="$(cat ./backup-url.txt)"
+DUPLICATI_PASSPHRASE="$(cat ./duplicati-passphrase.txt)"
+
+output=$(duplicati-cli backup "$URL" \
+  "/var/lib/jellyfin" \
+  "/etc/jellyfin" \
+  --dblock-size=50mb \
+  --backup-name="jellyfin" \
+  --passphrase="$DUPLICATI_PASSPHRASE" \
+  --retention-policy="1W:1D,4W:1W,12M:1M" | tee /dev/tty | tail -n 10)
 
 echo "Done: $output"
 send_discord_notification "$output"
